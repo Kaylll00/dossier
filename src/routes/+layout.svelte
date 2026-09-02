ce5851fbf422b1f077ac4a18a967d671f8e2485f
Earl Kyle A. Urdas <earlkayl111@gmail.com>
2026-09-02 09:47:18 +0800

    fix: structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { ensureSession } from '$lib/appwrite';
 	import '@fontsource-variable/fraunces';
 	import '@fontsource-variable/inter';
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	onMount(() => {
+		ensureSession();
+	});
 </script>
 
 <svelte:head>
